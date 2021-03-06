44c6cdb23d9d26e18d04595473e2bf9dfea767bf
Rae Abunahla <rae.abunahla@Raes-MacBook-Pro.local>
2021-03-05 20:51:50 -0500

    Addition of delete(), findMinimum(), maximumDepth(), longestPathLeafToLeaf(), highestCommonNode() implementations

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,6 +1,11 @@
+/**
+* BinarySearchTree class for Integers where the
+* smaller or equal values residing in the left nodes and
+* larger values residing in the right nodes.
+*/
 public class BinarySearchTree {
 
-    // Global variable:
+    // Our global private root:
     private Node root;
 
     // We use a private nested class becuase we only want BinaryTree's to access the node, and we want to be able to access the static variables across the class.
@@ -15,15 +20,16 @@ public class BinarySearchTree {
             rightChild = null;
         }
     }
-
-    // Variables
     
     // Constructor:
     public void BinarySearchTree() {
         root = null;
     }
 
-    // Insertion
+    /**
+    * This method inserts a value into the appropriate location 
+    * in the Binary Search Tree:
+    */
     public void insert(int value) {
         root = insertNode(root, value);
     }
@@ -31,7 +37,6 @@ public class BinarySearchTree {
     private Node insertNode(Node node, int value) {
         if (node == null) {
             node = new Node(value);
-            System.out.println(node.value);
         } else if (value <= node.value) {
             node.leftChild = insertNode(node.leftChild, value);
         } else {
@@ -41,9 +46,100 @@ public class BinarySearchTree {
         return node;
     }
 
-    // Deletion
+    /**
+    * This method deletes an existing value from the Binary Search 
+    * Tree and re-aligns the appropriate tree nodes.
+    */
+    public void delete(int value) {
+        if (contains(value)) {
+            root = deleteNode(root, value);
+        }
+    }
+
+    private Node deleteNode(Node node, int value) {
+       if (node.value == value) {
+           if (node.leftChild == null) {
+               node = node.rightChild;
+           } else if (node.rightChild == null) {
+                node = node.leftChild;
+           } else {
+               int min = this.findMinimum(node.rightChild);
+               deleteNode(node, min);
+               Node newNode = new Node(min);
+               newNode.leftChild = node.leftChild;
+               newNode.rightChild = node.rightChild;
+               node = newNode;
+           }
+       } else {
+           if (value <= node.value) {
+                node.leftChild = deleteNode(node.leftChild, value);
+           } else {
+                node.rightChild = deleteNode(node.rightChild, value);
+           }
+       }
+
+        return node;
+    }
+
 
     // Look up function:
+    public boolean contains(int value) {
+        return contains(root, value);
+    }
+
+    private boolean contains(Node node, int value) {
+        if (node == null) {
+            return false;
+        } else if (node.value == value) {
+            return true;
+        } else {
+            if (value <= node.value) {
+                return contains(node.leftChild, value);
+            } else {
+                return contains(node.rightChild, value);
+            }
+        }
+    }
+
+    public int longestPathLeaftoLeaf(int value1, int value2) {
+        Node commonNode = highestCommonNode(root, value1, value2);
+        System.out.println("common node is: " + commonNode.value);
+        int depth1 = depthOfNode(commonNode, value1);
+        int depth2 = depthOfNode(commonNode, value2);
+        return depth1+depth2+1;
+    }
+
+    /**
+    * Assumes value1 < value 2
+     */
+    private Node highestCommonNode(Node node, int value1, int value2) {
+        if (contains(node.leftChild, value1) && contains(node.rightChild, value2)) {
+            return node;
+        } else {
+            if (value1 <= node.value && value2 <= node.value) {
+                return highestCommonNode(node.leftChild, value1, value2);
+            } else {
+                return highestCommonNode(node.rightChild, value1, value2);
+            }
+        }
+        
+    }
+
+    public int depthOfNode(int value) {
+        return depthOfNode(root, value);
+    }
+
+    private int depthOfNode(Node node, int value) {
+        if (node.value == value) {
+            return 0;
+        } else {
+            if (value <= node.value) {
+                return depthOfNode(node.leftChild, value) + 1;
+            } else {
+                return depthOfNode(node.rightChild, value) + 1;
+            }
+        }
+    }
 
     // Size function:
 
@@ -53,9 +149,10 @@ public class BinarySearchTree {
 
     // Maximum value function:
 
-    // Print all orders.
-    // In-Order:
-
+    /**
+    * Prints out the Integer values of the Binary Search Tree 
+    * in order.
+    */
     public void printInOrder() {
         printInOrder(root);
     }
@@ -70,6 +167,11 @@ public class BinarySearchTree {
         printInOrder(node.rightChild);
     }
 
+
+    /**
+    * Prints out the Integer values of the Binary Search Tree 
+    * in post-order.
+    */
     public void printPostOrder() {
         printPostOrder(root);
     }
@@ -82,11 +184,38 @@ public class BinarySearchTree {
         printPostOrder(node.leftChild);
         printPostOrder(node.rightChild);
         System.out.print(node.value + "  ");
+    }
 
+    /**
+    * Prints out the Integer values of the Binary Search Tree 
+    * in pre-order.
+    */
+    public void printPreOrder() {
+        printPostOrder(root);
     }
     
+    private void printPreOrder(Node node) {
+        if (node == null) {
+            return;
+        }
 
-    // Ouptut:
+        System.out.print(node.value + "  ");
+        printPostOrder(node.leftChild);
+        printPostOrder(node.rightChild);
+    }
+
+    /**
+     
+    */
+    public int findMinimum(Node node) {
+        if (node.leftChild == null) {
+            return node.value;
+        } else {
+            return findMinimum(node.leftChild);
+        }
+    }
+
+    // Testing the methods above:
     public static void main(String[] args) {
         BinarySearchTree tester = new BinarySearchTree();
         tester.insert(3);
@@ -95,6 +224,19 @@ public class BinarySearchTree {
         tester.insert(1);
         tester.insert(4);
         tester.insert(-5);
+        tester.insert(-9);
+        tester.insert(7);
+        tester.insert(2);
+        tester.insert(14);
+        tester.insert(0);
+        tester.insert(12);
         tester.printInOrder();
+        tester.delete(-10);
+        System.out.println(" ");
+        tester.printInOrder();
+        System.out.println(tester.contains(4));
+        System.out.println(tester.contains(-4));
+        System.out.println(tester.longestPathLeaftoLeaf(-5,6));
+        System.out.println(tester.longestPathLeaftoLeaf(-9,0));
     }
 }
