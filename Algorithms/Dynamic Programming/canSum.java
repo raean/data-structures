ecde264206b21ff7feb39eb32582b9fb70f14ffe
Rae Abunahla <raghebabn@gmail.com>
2021-03-25 00:06:45 -0400

    canSum memo-ized solution

--- Algorithms/Dynamic Programming/canSum.java
+++ Algorithms/Dynamic Programming/canSum.java
@@ -17,6 +17,29 @@ public class canSum {
         }
     }
 
+    public static boolean canSumMemod(int value, int[] parts, boolean[] memo) {
+
+        if (memo[value]) {
+            return memo[value];
+        }
+
+        if (value == 0) {
+            return true;
+        } else if (value < 0) {
+            return false;
+        } else {
+             for (int i = 0 ; i < parts.length ; i++) {
+                 int remainder = value - parts[i];
+                 if (canSumMemod(remainder, parts, memo)) {
+                    memo[value] = true;
+                    return memo[value];
+                 }
+             }
+             memo[value] = false;
+            return memo[value];
+        }
+    }
+
     // Tests:
     public static void main(String[] args) {
         int[] t1 = {2, 3};
@@ -25,6 +48,8 @@ public class canSum {
         System.out.println(canSumRec(7, t2));
         int[] t3 = {2, 4};
         System.out.println(canSumRec(7, t3));
+
+       
 
     }
 }
